4dcd793015e88179c9e87902d982221ac93b3b01
Hoang Nguyen <ngvhoang96@gmail.com>
2021-10-19 15:36:59 -0500

    Propose test cases to the API

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 
 const app = express();
+const reservavtions = [
+	{ reservationID: 1, tableNumber: 14, isReserved: false },
+];
 
 app.get("/", (req, res) => {
 	res.send("Hello");
+});
+
+app.get("/API/reservations/", (req, res) => {
+	res.send(reservavtions);
 });
 
 export default app;
--- index.test.js
+++ index.test.js
@@ -2,13 +2,62 @@ import { expect } from "@jest/globals";
 import request from "supertest";
 import app from ".";
 
-describe("express test", () => {
+//THE DATABASE HAS 4 MOST SQL COMMAND
+//SELECT * FROM TABLE
+//INSERT INTO TABLE VALUES(*)
+//UPDATE TABLE SET * WHERE ID = ""
+//DELETE FROM * WHERE ID = ""
+
+describe("Test SELECT FROM TABLE", () => {
 	test("GET / return welcome message", async () => {
 		return request(app)
 			.get("/")
-			.expect(200)
 			.then((response) => {
 				expect(response.text).toBe("Hello");
 			});
 	});
+
+	test("GET /API/reservations return in JSON format", async () => {
+		return request(app)
+			.get("/API/reservations/")
+			.then((response) => {
+				expect(response.type).toBe("application/json");
+			});
+	});
+
+	test("GET /API/reservations return array with table number and isReserved boolean", async () => {
+		return request(app)
+			.get("/API/reservations/")
+			.then((response) => {
+				expect(response.body).toEqual(
+					expect.arrayContaining([
+						expect.objectContaining({
+							reservationID: expect.any(Number),
+							tableNumber: expect.any(Number),
+							isReserved: expect.any(Boolean),
+						}),
+					])
+				);
+			});
+	});
+
+	test("GET /API/reservations/id return a reservation", async () => {});
+});
+
+describe("Test INSERT INTO TABLE VALUES", () => {
+	test("POST /API/reservations/ require the body with proper format no ID", async () => {});
+
+	test("POST /API/reservations/ create a new reservation and return new ID", async () => {});
+});
+
+describe("Test UPDATE TABLE WHERE ID", () => {
+	test("PUT /API/reservation/id update the reservation", async () => {});
+
+	test("PUT /API/reservation/id returns 404 on invalid ID", async () => {});
+});
+
+describe("Test DELETE FROM TABLE WHERE ID", () => {
+	test("DELETE /API/reservation/id removes the reservation", async () => {});
+
+	test("DELETE API/reservation/id returns 404 on invalid ID", async () => {});
 });
