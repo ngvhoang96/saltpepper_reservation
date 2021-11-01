8db6c25cb5debb4eea1ea622db0fa014bfd0fa19
Hoang Nguyen <ngvhoang96@gmail.com>
2021-11-01 14:29:37 -0500

    Repair build script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongooseConnect from "./mongoClient.js";
 import reservation from "./routes/api/reservation.js";
-
+import path from "path";
 const app = express();
 
 await mongooseConnect();
@@ -9,5 +9,13 @@ await mongooseConnect();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/api/reservation", reservation);
+
+if (process.env.NODE_EVN === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 export default app;
