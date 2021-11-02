1af5a2d4808646228337c0cf22e8675f0b21ed4f
Hoang Nguyen <ngvhoang96@gmail.com>
2021-11-02 05:15:29 -0500

    Reimplement mongo-memory-server

--- mongoClient.js
+++ mongoClient.js
@@ -1,8 +1,21 @@
 import mongoose from "mongoose";
 import config from "./config.json";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 const mongooseConnect = async () => {
-	await mongoose.connect(config.mongoURI);
+	await mongoose.disconnect();
+	if (process.env.NODE_ENV === "test") {
+		console.log("IN TESTING ENVIRONMENT");
+		const mockMongoose = await MongoMemoryServer.create({
+			instance: {
+				dbName: "reservation",
+				dbPath: "/private/tmp/mongo-mem-1",
+			},
+		});
+		await mongoose.connect(mockMongoose.getUri());
+	} else {
+		await mongoose.connect(config.mongoURI);
+	}
 };
 
 export default mongooseConnect;
