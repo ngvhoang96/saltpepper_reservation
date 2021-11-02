afcf11f833e7d359d09974149c7054d597842b19
Hoang Nguyen <ngvhoang96@gmail.com>
2021-11-02 03:07:22 -0500

    Fix Heroku postbuild

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ import reservation from "./routes/api/reservation.js";
 import path from "path";
 const app = express();
 
-await mongooseConnect();
-
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/api/reservation", reservation);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static("client/build/"));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
