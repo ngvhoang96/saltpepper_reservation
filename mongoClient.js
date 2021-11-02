75a059db4c6e99c3a7e0c5d8c664a5621c45af20
Hoang Nguyen <ngvhoang96@gmail.com>
2021-11-02 04:28:58 -0500

    remove testing dependency mongodb-memory-server

--- mongoClient.js
+++ mongoClient.js
@@ -1,21 +1,8 @@
 import mongoose from "mongoose";
 import config from "./config.json";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 const mongooseConnect = async () => {
-	await mongoose.disconnect();
-	if (process.env.NODE_ENV === "test") {
-		console.log("IN TESTING ENVIRONMENT");
-		const mockMongoose = await MongoMemoryServer.create({
-			instance: {
-				dbName: "reservation",
-				dbPath: "/private/tmp/mongo-mem-1",
-			},
-		});
-		await mongoose.connect(mockMongoose.getUri());
-	} else {
-		await mongoose.connect(config.mongoURI);
-	}
+	await mongoose.connect(config.mongoURI);
 };
 
 export default mongooseConnect;
