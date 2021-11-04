1d611672181a0a19f726b9d6b9be91cf7c820cee
Hector <hector.angeles.school@gmail.com>
2021-11-04 18:43:13 -0500

    Added a modal and worked on database axios linking, work in progress

--- client/src/components/ReservationList.jsx
+++ client/src/components/ReservationList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "react-calendar/dist/Calendar.css";
-import { Alert, Button } from "reactstrap";
+import { Alert, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {useState } from "react";
+import axios from "axios";
 
 export default function ReservationList(props) {
 	const { reservation } = props;
+	const [modalIsopen, setModalIsOpen] = useState(false)
 	return (
 		<div>
 			{reservation.map((r) => {
 				return (
+					<div>
 					<Alert key={r._id} color="secondary">
 						Table {r.tableNumber}
 						{" ("}
@@ -16,10 +20,39 @@ export default function ReservationList(props) {
 						<Button
 							color={r.isReserved ? "secondary" : "danger"}
 							disabled={r.isReserved}
+							id = {r.tableNumber.toString()}
+							onClick={() =>setModalIsOpen(true)}
 						>
 							{r.hour}
 						</Button>
 					</Alert>
+
+					<Modal
+							isOpen = {modalIsopen}
+							
+						>
+							<ModalHeader>
+							Reserve This Time
+							</ModalHeader>
+							<ModalBody>
+							Are you sure you want to reserve this time?
+							</ModalBody>
+							<ModalFooter>
+							<Button
+								color="primary"
+								onClick={() => axios.put("/api/reservation/" + 2,{isReserved: true})}
+								// Still working on this and am having trouble getting the right number to send to the database to update.
+								//hard coded table number two to update to true.
+							>
+								Reserve
+							</Button>
+							{' '}
+							<Button onClick={() => setModalIsOpen(false)}>
+								Cancel
+							</Button>
+							</ModalFooter>
+						</Modal>
+					</div>
 				);
 			})}
 		</div>
