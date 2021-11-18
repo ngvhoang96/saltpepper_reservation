f82e921e689162b47e4cf632968dd46e4daa1647
Hector <hector.angeles.school@gmail.com>
2021-11-17 21:08:55 -0600

    Preventing customers from reserving a table in the past

--- client/src/components/TableView.jsx
+++ client/src/components/TableView.jsx
@@ -8,84 +8,88 @@ import { ReservationContext } from "../contextProvider/ReservationContext";
 import ChooseGuestSize from "./ChooseGuestSize";
 
 export const TableView = () => {
-	const [state, setState] = useContext(ReservationContext);
-	const [date, setDate] = useState(new Date());
-	const [tables, setTables] = useState([]);
+  const [state, setState] = useContext(ReservationContext);
+  const [date, setDate] = useState(new Date());
+  const [tables, setTables] = useState([]);
 
-	//formate date from "Nov 6 2021 Wed ..."
-	//to "11-6-2021"
-	const formatDate = (theDate) => {
-		const date = new Date(theDate);
-		return date.toLocaleDateString("en-US").replace(/\//g, "-");
-	};
+  //formate date from "Nov 6 2021 Wed ..."
+  //to "11-6-2021"
+  const formatDate = (theDate) => {
+    const date = new Date(theDate);
+    return date.toLocaleDateString("en-US").replace(/\//g, "-");
+  };
 
-	const modalText = `You selected ${state.selectedHour} on ${state.selectedDate} from table ${state.selectedTable}`;
+  const modalText = `You selected ${state.selectedHour} on ${state.selectedDate} from table ${state.selectedTable}`;
 
-	//fetch table from db
-	useEffect(() => {
-		const fetchTables = async () => {
-			return await axios.get("/api/table/");
-		};
+  //fetch table from db
+  useEffect(() => {
+    const fetchTables = async () => {
+      return await axios.get("/api/table/");
+    };
 
-		if (tables.length === 0) {
-			fetchTables().then((response) => {
-				const listOfTables = response.data;
-				setTables(listOfTables.slice());
-			});
-		}
-	}, [tables]);
+    if (tables.length === 0) {
+      fetchTables().then((response) => {
+        const listOfTables = response.data;
+        setTables(listOfTables.slice());
+      });
+    }
+  }, [tables]);
 
-	if (state.viewMode === "TableView") {
-		return (
-			<div>
-				<Calendar onChange={(date) => setDate(date)} value={date} />
+  if (state.viewMode === "TableView") {
+    return (
+      <div>
+        <Calendar
+          onChange={(date) => setDate(date)}
+          value={date}
+          minDate={new Date()}
+        />
 
-				<ChooseGuestSize />
+        <ChooseGuestSize />
 
-				{state.numberOfGuest &&
-				state.numberOfGuest > (state.chosenGuestSize || 0)
-					? tables.map((table) => {
-							return (
-								<TableViewByDate
-									key={table.tableNumber}
-									table={table}
-									date={formatDate(date)}
-								/>
-							);
-					  })
-					: ""}
+        {state.numberOfGuest &&
+        state.numberOfGuest > (state.chosenGuestSize || 0)
+          ? tables.map((table) => {
+              return (
+                <TableViewByDate
+                  key={table.tableNumber}
+                  table={table}
+                  date={formatDate(date)}
+                />
+              );
+            })
+          : ""}
 
-				<Modal
-					centered={true}
-					isOpen={state.showModal}
-					toggle={() => setState({ ...state, showModal: false })}
-				>
-					<ModalHeader>Please confirm</ModalHeader>
-					<ModalBody>{modalText}</ModalBody>
-					<ModalFooter>
-						<Button
-							color="secondary"
-							onClick={() => setState({ ...state, showModal: false })}
-						>
-							No
-						</Button>
-						<Button
-							color="danger"
-							onClick={() =>
-								setState({
-									...state,
-									showModal: false,
-									viewMode: "ReservationForm",
-								})
-							}
-						>
-							Yes
-						</Button>
-					</ModalFooter>
-				</Modal>
-			</div>
-		);
-	} else {
-		return null;
-	}
+        <Modal
+          centered={true}
+          isOpen={state.showModal}
+          toggle={() => setState({ ...state, showModal: false })}
+        >
+          <ModalHeader>Please confirm</ModalHeader>
+          <ModalBody>{modalText}</ModalBody>
+          <ModalFooter>
+            <Button
+              color="secondary"
+              onClick={() => setState({ ...state, showModal: false })}
+            >
+              No
+            </Button>
+            <Button
+              color="danger"
+              onClick={() =>
+                setState({
+                  ...state,
+                  showModal: false,
+                  viewMode: "ReservationForm",
+                })
+              }
+            >
+              Yes
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
