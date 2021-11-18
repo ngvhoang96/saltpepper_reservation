0d6e462e0007482858f7d3f6085b7cc6a46c10f9
Hector <hector.angeles.school@gmail.com>
2021-11-18 01:36:48 -0600

    Changed the Nav Bar Options to better reflect the navigation

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,36 +1,80 @@
 import { useState } from "react";
 import {
-	Navbar,
-	NavLink,
-	NavbarBrand,
-	NavbarToggler,
-	Nav,
-	NavItem,
-	Collapse,
+  Navbar,
+  NavLink,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  Collapse,
+  Button,
 } from "reactstrap";
+import { Login } from "./Utility/Login";
+import { BrowserRouter } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export const NavigationBar = () => {
-	const [isOpen, toggleNavBar] = useState(false);
+  const [isOpen, toggleNavBar] = useState(false);
+  const dispatch = useDispatch();
+  const customerReducer = useSelector((state) => state.customerReducer);
+  const handleLoggedout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem("_token");
+    //redux
+    dispatch({ type: "action.logout" });
+  };
 
-	return (
-		<Navbar color="faded m-3" light>
-			<NavbarBrand href="/" className="mr-auto">
-				Salt Pepper
-			</NavbarBrand>
-			<NavbarToggler onClick={() => toggleNavBar(!isOpen)} className="mr-2" />
-			<Collapse isOpen={isOpen} navbar>
-				<Nav navbar>
-					<NavItem>
-						<NavLink href="/account/">Log in</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="/register/">Sign up</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="/reservation">Reserve a table</NavLink>
-					</NavItem>
-				</Nav>
-			</Collapse>
-		</Navbar>
-	);
+  if (customerReducer?.isLoggedIn) {
+    return (
+      <Navbar color="faded m-3" light>
+        <NavbarBrand href="/" className="mr-auto">
+          Salt Pepper
+        </NavbarBrand>
+        <NavbarToggler onClick={() => toggleNavBar(!isOpen)} className="mr-2" />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/account/">My Account</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/reservation">Reserve a table</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/" onClick={handleLoggedout}>
+                Log Out
+              </NavLink>
+            </NavItem>
+            {/* <Button className="mt-3" onClick={handleLoggedout}>
+              Log out
+            </Button> */}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar color="faded m-3" light>
+        <NavbarBrand href="/" className="mr-auto">
+          Salt Pepper
+        </NavbarBrand>
+        <NavbarToggler onClick={() => toggleNavBar(!isOpen)} className="mr-2" />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <BrowserRouter>
+                <Login />
+              </BrowserRouter>
+            </NavItem>
+
+            <NavItem>
+              <NavLink href="/register/">Sign up</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/reservation">Reserve a table</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
 };
--- client/src/components/Utility/Login.jsx
+++ client/src/components/Utility/Login.jsx
@@ -1,12 +1,12 @@
 import {
-	Form,
-	FormGroup,
-	Input,
-	Button,
-	Modal,
-	ModalBody,
-	ModalHeader,
-	ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
 } from "reactstrap";
 import React, { useState, useContext } from "react";
 import { NotifyPanel } from "./NotifyPanel";
@@ -18,113 +18,113 @@ import { Link } from "react-router-dom";
 const LoginContext = React.createContext({});
 
 export const Login = () => {
-	//redux
-	const dispatch = useDispatch();
-	//
-	const [state, setState] = useState({
-		isLoggedIn: false,
-		showModal: false,
-		email: "",
-		password: "",
-		errorList: [],
-	});
+  //redux
+  const dispatch = useDispatch();
+  //
+  const [state, setState] = useState({
+    isLoggedIn: false,
+    showModal: false,
+    email: "",
+    password: "",
+    errorList: [],
+  });
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		axios
-			.post("/api/customer/login", {
-				email: state.email,
-				password: state.password,
-			})
-			.then(({ data }) => {
-				setState({ ...state, errorList: [], isLoggedIn: true });
-				localStorage.setItem("_token", data.token);
-				//redux
-				dispatch(actionLogin);
-			})
-			.catch(({ response }) => {
-				setState({ ...state, errorList: [response.data.error] });
-			});
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("/api/customer/login", {
+        email: state.email,
+        password: state.password,
+      })
+      .then(({ data }) => {
+        setState({ ...state, errorList: [], isLoggedIn: true });
+        localStorage.setItem("_token", data.token);
+        //redux
+        dispatch(actionLogin);
+      })
+      .catch(({ response }) => {
+        setState({ ...state, errorList: [response.data.error] });
+      });
+  };
 
-	if (state.isLoggedIn) {
-		return null;
-	} else {
-		return (
-			<LoginContext.Provider value={[state, setState]}>
-				<Button onClick={() => setState({ ...state, showModal: true })}>
-					Sign In
-				</Button>
-				<LoginForm onSubmit={handleSubmit} />
-			</LoginContext.Provider>
-		);
-	}
+  if (state.isLoggedIn) {
+    return null;
+  } else {
+    return (
+      <LoginContext.Provider value={[state, setState]}>
+        <Button onClick={() => setState({ ...state, showModal: true })}>
+          Sign In
+        </Button>
+        <LoginForm onSubmit={handleSubmit} />
+      </LoginContext.Provider>
+    );
+  }
 };
 
 const LoginForm = ({ onSubmit }) => {
-	const [state, setState] = useContext(LoginContext);
+  const [state, setState] = useContext(LoginContext);
 
-	return (
-		<Modal isOpen={state.showModal} centered={true} className="modal-lg">
-			<ModalHeader className="justify-content-center">Sign in</ModalHeader>
-			<Form onSubmit={(event) => onSubmit(event)}>
-				<ModalBody>
-					<div className="container d-flex p-2">
-						<div className="row">
-							<div className="col text-center">
-								<img
-									className="rounded-circle mt-4"
-									src="https://www.nowplayingnashville.com/wp-content/uploads/sites/www.nowplayingnashville.com/images/2018/10/salt-pepper.png"
-									width="75%"
-								/>
-							</div>
+  return (
+    <Modal isOpen={state.showModal} centered={true} className="modal-lg">
+      <ModalHeader className="justify-content-center">Sign in</ModalHeader>
+      <Form onSubmit={(event) => onSubmit(event)}>
+        <ModalBody>
+          <div className="container d-flex p-2">
+            <div className="row">
+              <div className="col text-center">
+                <img
+                  className="rounded-circle mt-4"
+                  src="https://www.nowplayingnashville.com/wp-content/uploads/sites/www.nowplayingnashville.com/images/2018/10/salt-pepper.png"
+                  width="75%"
+                />
+              </div>
 
-							<div className="col align-self-center">
-								<NotifyPanel>{state.errorList}</NotifyPanel>
-								<div className="text-center">
-									<img
-										className="mb-2"
-										src="https://screenshots.imgix.net/mui-org/material-ui-icons/lock-outlined/~v=3.9.2/f4cb3020-77a2-49c2-8d0b-d2fc53d3a3f0.png?ixlib=js-1.2.0&s=45047390e8a4936dd989837eadff3bb4"
-										width="10%"
-									/>
-								</div>
-								<FormGroup>
-									<Input
-										placeholder="Email"
-										type="email"
-										value={state.email}
-										onChange={(event) =>
-											setState({ ...state, email: event.target.value })
-										}
-										name="email"
-										className="mb-2"
-									/>
-								</FormGroup>
-								<FormGroup>
-									<Input
-										placeholder="Password"
-										type="password"
-										value={state.password}
-										onChange={(event) =>
-											setState({ ...state, password: event.target.value })
-										}
-										name="password"
-										className="mb-2"
-									/>
-								</FormGroup>
-								<div>Dont Have an Account?</div>
-								<Link to="/register">Click Here to Sign Up</Link>
-							</div>
-						</div>
-					</div>
-				</ModalBody>
-				<ModalFooter>
-					<Button onClick={() => setState({ ...state, showModal: false })}>
-						Cancel
-					</Button>
-					<Button color="danger">Submit</Button>
-				</ModalFooter>
-			</Form>
-		</Modal>
-	);
+              <div className="col align-self-center">
+                <NotifyPanel>{state.errorList}</NotifyPanel>
+                <div className="text-center">
+                  <img
+                    className="mb-2"
+                    src="https://screenshots.imgix.net/mui-org/material-ui-icons/lock-outlined/~v=3.9.2/f4cb3020-77a2-49c2-8d0b-d2fc53d3a3f0.png?ixlib=js-1.2.0&s=45047390e8a4936dd989837eadff3bb4"
+                    width="10%"
+                  />
+                </div>
+                <FormGroup>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    value={state.email}
+                    onChange={(event) =>
+                      setState({ ...state, email: event.target.value })
+                    }
+                    name="email"
+                    className="mb-2"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    value={state.password}
+                    onChange={(event) =>
+                      setState({ ...state, password: event.target.value })
+                    }
+                    name="password"
+                    className="mb-2"
+                  />
+                </FormGroup>
+                <div>Dont Have an Account?</div>
+                <Link to="/register">Click Here to Sign Up</Link>
+              </div>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => setState({ ...state, showModal: false })}>
+            Cancel
+          </Button>
+          <Button color="danger">Submit</Button>
+        </ModalFooter>
+      </Form>
+    </Modal>
+  );
 };
