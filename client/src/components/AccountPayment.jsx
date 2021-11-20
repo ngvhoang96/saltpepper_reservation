a6d8e2962ee799dca88546b3ac2dd54481bd697f
Entropy-1 <78392387+Entropy-1@users.noreply.github.com>
2021-11-20 00:05:01 -0600

    Added Payments card for reservations, still wokring on UI and Design.

--- client/src/components/AccountPayment.jsx
+++ client/src/components/AccountPayment.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardHeader,
+} from "reactstrap";
+import { AccountContext } from "../contextProvider/AccountContext";
 
 function AccountPayment() {
-	return <div>All payments belong to this account should be listed here</div>;
+  const [state, setState] = useContext(AccountContext);
+
+  return (
+    <div>
+      {state.payments?.map((payments) => {
+        return (
+          <Card>
+            <CardHeader>
+              <div class="row">
+                <div class="col">
+                  <CardTitle tag="h5">{payments.description}</CardTitle>
+                </div>
+                <div class="col">
+                  <CardSubtitle className="mb-2 text-muted float-end">
+                    Reservation# {payments._id}
+                  </CardSubtitle>
+                </div>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <div>
+                <CardTitle tag="h7">Total: ${payments.amount}</CardTitle>
+              </div>
+              <CardText tag="h7">
+                Reservervation placed on {payments.date}
+              </CardText>
+            </CardBody>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default AccountPayment;
