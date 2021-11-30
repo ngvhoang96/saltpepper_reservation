3914ca0c2b039e28592d1581975c8273fdbbe4f0
Entropy-1 <78392387+Entropy-1@users.noreply.github.com>
2021-11-30 00:26:12 -0600

    Asthetic Changes to HomeView and Account View

--- client/src/components/AccountView.jsx
+++ client/src/components/AccountView.jsx
@@ -4,35 +4,66 @@ import { AccountInfo } from "./AccountInfo";
 import { AccountContext } from "../contextProvider/AccountContext";
 import { NotifyPanel } from "./Utility/NotifyPanel";
 import { useDispatch, useSelector } from "react-redux";
+import avatar from "../assets/avatar.png";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardHeader,
+  Container,
+} from "reactstrap";
 
 export const AccountView = () => {
-	//redux
-	const dispatch = useDispatch();
-	const customerReducer = useSelector((state) => state.customerReducer);
-	//
-	const [state, setState] = useState({});
+  //redux
+  const dispatch = useDispatch();
+  const customerReducer = useSelector((state) => state.customerReducer);
+  //
+  const [state, setState] = useState({});
 
-	useEffect(() => {
-		if (customerReducer) {
-			setState((s) => ({ ...s, ...customerReducer }));
-		}
-	}, [customerReducer]);
+  useEffect(() => {
+    if (customerReducer) {
+      setState((s) => ({ ...s, ...customerReducer }));
+    }
+  }, [customerReducer]);
 
-	const handleLoggedout = (event) => {
-		event.preventDefault();
-		localStorage.removeItem("_token");
-		//redux
-		dispatch({ type: "action.logout" });
-	};
+  const handleLoggedout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem("_token");
+    //redux
+    dispatch({ type: "action.logout" });
+  };
 
-	if (customerReducer?.isLoggedIn) {
-		return (
-			<AccountContext.Provider value={[state, setState]}>
-				<NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
-				<AccountInfo onLoggedOut={handleLoggedout} />
-			</AccountContext.Provider>
-		);
-	} else {
-		return <Login />;
-	}
+  if (customerReducer?.isLoggedIn) {
+    return (
+      <AccountContext.Provider value={[state, setState]}>
+        <NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
+        <AccountInfo onLoggedOut={handleLoggedout} />
+      </AccountContext.Provider>
+    );
+  } else {
+    return (
+      <div class="row justify-content-md-center">
+        <div class="col col-6 ">
+          <div className="text-center my-auto d-none d-sm-block">
+            <img
+              alt="salt-pepper"
+              className="rounded-circle"
+              src={avatar}
+              width="15%"
+            />
+          </div>
+          <h2 class="display-6 text-center">
+            It looks like you're not logged in.
+          </h2>
+          <p className="lead text-center">Please click below to sign in</p>
+          <div class="row justify-content-md-center">
+            <div class="col col-lg-auto">
+              <Login />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
--- client/src/components/HomeView.jsx
+++ client/src/components/HomeView.jsx
@@ -1,23 +1,36 @@
 import { Link } from "react-router-dom";
 import { Container, Jumbotron, Button } from "reactstrap";
+import saltPepperLogo from "../assets/saltpepper.jpg";
 
 export const HomeView = () => {
-	return (
-		<Container className="themed-container">
-			<Jumbotron>
-				<h1 className="display-3">Hello customers!</h1>
-				<p className="lead">
-					Welcome to Salt Pepper reservation, we would like to introduce to you
-					our new look of the website.
-				</p>
-				<hr className="my-2" />
-				<p>Click now to make a reservation!</p>
-				<p className="lead">
-					<Link to={"/reservation"}>
-						<Button color="danger">Reserve Now</Button>
-					</Link>
-				</p>
-			</Jumbotron>
-		</Container>
-	);
+  return (
+    <Container className="themed-container">
+      <Jumbotron>
+        <div className="row align-items-center">
+          <div className="col my-auto d-none d-sm-block">
+            <img
+              alt="salt-pepper"
+              className="rounded-circle"
+              src={saltPepperLogo}
+              width="75%"
+            />
+          </div>
+          <div className="col my-auto">
+            <h1 className="display-3">Hello Customers!</h1>
+            <p className="lead">
+              Welcome to Salt Pepper reservation, we would like to introduce to
+              you our new look of the website.
+            </p>
+            <hr className="my-2" />
+            <p>Click now to make a reservation!</p>
+            <p className="lead">
+              <Link to={"/reservation"}>
+                <Button color="danger">Reserve Now</Button>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </Jumbotron>
+    </Container>
+  );
 };
