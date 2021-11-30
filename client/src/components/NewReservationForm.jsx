754c414ccf6d41a0f891370a2d36fe7770031885
Entropy-1 <78392387+Entropy-1@users.noreply.github.com>
2021-11-30 02:07:05 -0600

    Added Email and Phone number validation in NewReservationForm

--- client/src/components/NewReservationForm.jsx
+++ client/src/components/NewReservationForm.jsx
@@ -1,139 +1,154 @@
 import { useContext, useState } from "react";
 import {
-	Button,
-	Input,
-	Form,
-	FormGroup,
-	Label,
-	Modal,
-	ModalBody,
-	ModalFooter,
+  Button,
+  Input,
+  Form,
+  FormGroup,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
 } from "reactstrap";
 import { ReservationContext } from "../contextProvider/ReservationContext";
 import { Login } from "./Utility/Login";
 import { SignUpView } from "./SignUpView";
 import { Link } from "react-router-dom";
+import validator from "validator";
 
 export const NewReservationForm = () => {
-	const [state, setState] = useContext(ReservationContext);
+  const [state, setState] = useContext(ReservationContext);
 
-	const proceed = (event) => {
-		event.preventDefault();
-		setState({ ...state, viewMode: "FinalizingView" });
-	};
+  const proceed = (event) => {
+    event.preventDefault();
+    if (validator.isEmail(state.email)) {
+      if (validator.isMobilePhone(state.phoneNumber)) {
+        setState({ ...state, viewMode: "FinalizingView" });
+      } else {
+        setState({
+          ...state,
+          notify: { type: "danger", msg: ["Phone Number Is Not Valid"] },
+        });
+      }
+    } else {
+      setState({
+        ...state,
+        notify: { type: "danger", msg: ["Email Is Not Valid"] },
+      });
+    }
+  };
 
-	if (state.viewMode === "ReservationForm") {
-		return (
-			<div>
-				<div className="mb-2">
-					<Button onClick={() => setState({ ...state, viewMode: "TableView" })}>
-						Change my mind
-					</Button>{" "}
-				</div>
-				<div className="mb-3 p-2">
-					<div className="row border-bottom border-gray py-2">
-						<div className="col-3">Date</div>
-						<div className="col">{state.selectedDate}</div>
-					</div>
-					<div className="row border-bottom border-gray py-2">
-						<div className="col-3">Hour</div>
-						<div className="col">{state.selectedHour}</div>
-					</div>
-					<div className="row border-bottom border-gray py-2">
-						<div className="col-3">Guests</div>
-						<div className="col">{state.numberOfGuest}</div>
-					</div>
-				</div>
-				<Form onSubmit={proceed}>
-					<FormGroup>
-						<Label>Name</Label>
-						<Input
-							className="mb-2"
-							value={state?.customerName || ""}
-							onChange={(event) => {
-								setState({ ...state, customerName: event.target.value });
-							}}
-							required
-							readOnly={state.isLoggedIn}
-						/>
-					</FormGroup>
+  if (state.viewMode === "ReservationForm") {
+    return (
+      <div>
+        <div className="mb-2">
+          <Button onClick={() => setState({ ...state, viewMode: "TableView" })}>
+            Change my mind
+          </Button>{" "}
+        </div>
+        <div className="mb-3 p-2">
+          <div className="row border-bottom border-gray py-2">
+            <div className="col-3">Date</div>
+            <div className="col">{state.selectedDate}</div>
+          </div>
+          <div className="row border-bottom border-gray py-2">
+            <div className="col-3">Hour</div>
+            <div className="col">{state.selectedHour}</div>
+          </div>
+          <div className="row border-bottom border-gray py-2">
+            <div className="col-3">Guests</div>
+            <div className="col">{state.numberOfGuest}</div>
+          </div>
+        </div>
+        <Form onSubmit={proceed}>
+          <FormGroup>
+            <Label>Name</Label>
+            <Input
+              className="mb-2"
+              value={state?.customerName || ""}
+              onChange={(event) => {
+                setState({ ...state, customerName: event.target.value });
+              }}
+              required
+              readOnly={state.isLoggedIn}
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label>Email</Label>
-						<Input
-							type="email"
-							className="mb-2"
-							value={state?.email || ""}
-							onChange={(event) => {
-								setState({ ...state, email: event.target.value });
-							}}
-							required
-							readOnly={state.isLoggedIn}
-						/>
-					</FormGroup>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              className="mb-2"
+              value={state?.email || ""}
+              onChange={(event) => {
+                setState({ ...state, email: event.target.value });
+              }}
+              required
+              readOnly={state.isLoggedIn}
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label>Phone number</Label>
-						<Input
-							type="number"
-							className="mb-2"
-							value={state?.phoneNumber || ""}
-							onChange={(event) => {
-								setState({ ...state, phoneNumber: event.target.value });
-							}}
-							required
-							readOnly={state.isLoggedIn}
-						/>
-					</FormGroup>
+          <FormGroup>
+            <Label>Phone number</Label>
+            <Input
+              type="number"
+              className="mb-2"
+              value={state?.phoneNumber || ""}
+              onChange={(event) => {
+                setState({ ...state, phoneNumber: event.target.value });
+              }}
+              required
+              readOnly={state.isLoggedIn}
+            />
+          </FormGroup>
 
-					{state.isLoggedIn ? (
-						<div className="fst-italic">
-							If you want to update your account information, please go to{" "}
-							<Link to="/account">My profile</Link>
-						</div>
-					) : (
-						""
-					)}
+          {state.isLoggedIn ? (
+            <div className="fst-italic">
+              If you want to update your account information, please go to{" "}
+              <Link to="/account">My profile</Link>
+            </div>
+          ) : (
+            ""
+          )}
 
-					<Button className="mt-3" color="danger">
-						Proceed {state.isLoggedIn ? "" : "as a guest"}
-					</Button>
-				</Form>
-				<SignUpSignIn show={!state.isLoggedIn} data={state} />
-			</div>
-		);
-	} else {
-		return null;
-	}
+          <Button className="mt-3" color="danger">
+            Proceed {state.isLoggedIn ? "" : "as a guest"}
+          </Button>
+        </Form>
+        <SignUpSignIn show={!state.isLoggedIn} data={state} />
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 const SignUpSignIn = ({ show, data }) => {
-	const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-	const toggleModal = () => {
-		setShowModal(!showModal);
-	};
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
-	if (show) {
-		return (
-			<div className="mt-3">
-				<span className="d-block mb-2">
-					Want to save your information for future booking?
-				</span>
-				<Button onClick={toggleModal}>Create new account</Button>
-				<span className="mx-2">or</span>
-				<Login />
-				<Modal isOpen={showModal}>
-					<ModalBody>
-						<SignUpView data={data} />
-					</ModalBody>
-					<ModalFooter>
-						<Button onClick={toggleModal}>Close</Button>
-					</ModalFooter>
-				</Modal>
-			</div>
-		);
-	} else {
-		return null;
-	}
+  if (show) {
+    return (
+      <div className="mt-3">
+        <span className="d-block mb-2">
+          Want to save your information for future booking?
+        </span>
+        <Button onClick={toggleModal}>Create new account</Button>
+        <span className="mx-2">or</span>
+        <Login />
+        <Modal isOpen={showModal}>
+          <ModalBody>
+            <SignUpView data={data} />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={toggleModal}>Close</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
--- client/src/components/SignUpView.jsx
+++ client/src/components/SignUpView.jsx
@@ -5,142 +5,143 @@ import { NotifyPanel } from "./Utility/NotifyPanel";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { actionFetchCustomerData } from "../redux_store/reducers/customerReducer";
+import validator from "validator";
 
 export const SignUpView = ({ data }) => {
-	const [state, setState] = useState(data || {});
-	const dispatch = useDispatch();
+  const [state, setState] = useState(data || {});
+  const dispatch = useDispatch();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (state.isSameAddress) {
-			setState({ ...state, billingAddress: state?.address || "" });
-		}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.isSameAddress) {
+      setState({ ...state, billingAddress: state?.address || "" });
+    }
 
-		const newCustomer = {
-			customerName: state.customerName || undefined,
-			email: state.email || undefined,
-			password: state.password || undefined,
-			phoneNumber: state.phoneNumber || undefined,
-			address: state.address || undefined,
-			billingAddress: state.billingAddress || undefined,
-		};
+    const newCustomer = {
+      customerName: state.customerName || undefined,
+      email: state.email || undefined,
+      password: state.password || undefined,
+      phoneNumber: state.phoneNumber || undefined,
+      address: state.address || undefined,
+      billingAddress: state.billingAddress || undefined,
+    };
 
-		axios
-			.post("/api/customer/", newCustomer)
-			.then(({ data }) => {
-				setState({
-					...state,
-					isSignedUp: true,
-					notify: { type: "success", msg: ["Account is created"] },
-				});
-				localStorage.setItem("_token", data.token);
-				//redux
-				dispatch(actionFetchCustomerData);
-			})
-			.catch((error) => {
-				setState({
-					...state,
-					notify: { type: "danger", msg: error.response.data },
-				});
-			});
-	};
+    axios
+      .post("/api/customer/", newCustomer)
+      .then(({ data }) => {
+        setState({
+          ...state,
+          isSignedUp: true,
+          notify: { type: "success", msg: ["Account is created"] },
+        });
+        localStorage.setItem("_token", data.token);
+        //redux
+        dispatch(actionFetchCustomerData);
+      })
+      .catch((error) => {
+        setState({
+          ...state,
+          notify: { type: "danger", msg: error.response.data },
+        });
+      });
+  };
 
-	if (state?.isSignedUp) {
-		return (
-			<div>
-				<NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
-				<h3>Thank you {state.customerName},</h3>
-				<Link to="/account">See account</Link>
-			</div>
-		);
-	} else {
-		return (
-			<div>
-				<h2 className=" mb-3">Create a new account</h2>
-				<NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
-				<Form onSubmit={handleSubmit}>
-					<FormGroup>
-						<Label for="customerName">Name</Label>
-						<Input
-							type="text"
-							id="customerName"
-							placeholder="John Doe"
-							value={state.customerName || ""}
-							onChange={(e) =>
-								setState({ ...state, customerName: e.target.value })
-							}
-						/>
-					</FormGroup>
+  if (state?.isSignedUp) {
+    return (
+      <div>
+        <NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
+        <h3>Thank you {state.customerName},</h3>
+        <Link to="/account">See account</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2 className=" mb-3">Create a new account</h2>
+        <NotifyPanel type={state.notify?.type}>{state.notify?.msg}</NotifyPanel>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="customerName">Name</Label>
+            <Input
+              type="text"
+              id="customerName"
+              placeholder="John Doe"
+              value={state.customerName || ""}
+              onChange={(e) =>
+                setState({ ...state, customerName: e.target.value })
+              }
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label for="customerEmail">Email</Label>
-						<Input
-							type="email"
-							id="customerEmail"
-							placeholder="name@example.com"
-							value={state.email || ""}
-							onChange={(e) => setState({ ...state, email: e.target.value })}
-						/>
-					</FormGroup>
+          <FormGroup>
+            <Label for="customerEmail">Email</Label>
+            <Input
+              type="email"
+              id="customerEmail"
+              placeholder="name@example.com"
+              value={state.email || ""}
+              onChange={(e) => setState({ ...state, email: e.target.value })}
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label for="customerPassword">Password</Label>
-						<Input
-							type="password"
-							id="customerPassword"
-							value={state.password || ""}
-							onChange={(e) => setState({ ...state, password: e.target.value })}
-						/>
-					</FormGroup>
+          <FormGroup>
+            <Label for="customerPassword">Password</Label>
+            <Input
+              type="password"
+              id="customerPassword"
+              value={state.password || ""}
+              onChange={(e) => setState({ ...state, password: e.target.value })}
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label for="phoneNumber">Phone number</Label>
-						<Input
-							id="phoneNumber"
-							value={state.phoneNumber || ""}
-							onChange={(e) =>
-								setState({ ...state, phoneNumber: e.target.value })
-							}
-						/>
-					</FormGroup>
+          <FormGroup>
+            <Label for="phoneNumber">Phone number</Label>
+            <Input
+              id="phoneNumber"
+              value={state.phoneNumber || ""}
+              onChange={(e) =>
+                setState({ ...state, phoneNumber: e.target.value })
+              }
+            />
+          </FormGroup>
 
-					<FormGroup>
-						<Label for="exampleText">Address</Label>
-						<Input
-							id="customerAddress"
-							value={state.address || ""}
-							onChange={(e) => setState({ ...state, address: e.target.value })}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Input
-							type="checkbox"
-							defaultChecked={!!state.isSameAddress}
-							onChange={() =>
-								setState({
-									...state,
-									isSameAddress: !state.isSameAddress,
-								})
-							}
-						/>
-						<Label>Billing Address is the same as Mailing Address</Label>
-					</FormGroup>
-					<FormGroup>
-						<Label>Billing Address</Label>
-						<Input
-							id="customerBillingAddress"
-							disabled={state.isSameAddress || false}
-							value={
-								state.isSameAddress ? state.address : state.billingAddress || ""
-							}
-							onChange={(e) =>
-								setState({ ...state, billingAddress: e.target.value })
-							}
-						/>
-					</FormGroup>
-					<Button color="danger">Submit</Button>
-				</Form>
-			</div>
-		);
-	}
+          <FormGroup>
+            <Label for="exampleText">Address</Label>
+            <Input
+              id="customerAddress"
+              value={state.address || ""}
+              onChange={(e) => setState({ ...state, address: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              type="checkbox"
+              defaultChecked={!!state.isSameAddress}
+              onChange={() =>
+                setState({
+                  ...state,
+                  isSameAddress: !state.isSameAddress,
+                })
+              }
+            />
+            <Label>Billing Address is the same as Mailing Address</Label>
+          </FormGroup>
+          <FormGroup>
+            <Label>Billing Address</Label>
+            <Input
+              id="customerBillingAddress"
+              disabled={state.isSameAddress || false}
+              value={
+                state.isSameAddress ? state.address : state.billingAddress || ""
+              }
+              onChange={(e) =>
+                setState({ ...state, billingAddress: e.target.value })
+              }
+            />
+          </FormGroup>
+          <Button color="danger">Submit</Button>
+        </Form>
+      </div>
+    );
+  }
 };
